0f1f951b0fafcada12c8eead7b8de7120a28dd04
Bianca Evila <biancaevila24@.com>
2025-12-08 22:05:37 -0300

    Changing menu

--- code/Menu.py
+++ code/Menu.py
@@ -19,8 +19,8 @@ class Menu:
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50, "Mountain", C_WHITE, ((WIN_WIDTH / 2), 70))
+            self.menu_text(50, "Shooter", C_WHITE, ((WIN_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
